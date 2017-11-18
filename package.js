f61b6265e51085cb1f2cfc340c45c4348668dbe5
Alex Manoel Ferreira Silva <windol@legytma.com.br>
2017-11-18 19:14:01 -0200

    Correção da url do git

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 	// Brief, one-line summary of the package.
 	summary: 'Meteor dynamic collections',
 	// URL to the Git repository containing the source code for this package.
-	git: 'https://bitbucket.org/Windol/platform-engine.git',
+	git: 'https://github.com/Windol/meteor-dynamic-collections.git',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'
